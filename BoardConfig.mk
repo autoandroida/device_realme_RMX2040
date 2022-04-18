9204287872bd3c82ff7860f0f5636890c7403f30
ksauraj <gitsauraj@gmail.com>
2022-04-18 11:14:11 +0000

    root_files: sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1607914664672-release-keys-Sun-Mar-20-09-50-27-CST-2022

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+DEVICE_PATH := device/realme/RMX2040
+BOARD_VENDOR := realme
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-03-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.2-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.gpu@1.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.health@2.1.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.neuralnetworks@1.3-service-mtk-gpu.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.wifi.hostapd.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.wifi@1.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/lights-mtk-default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_android.hardware.drm@1.3-service.clearkey.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_android.hardware.drm@1.3-service.widevine.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_soter.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/oplus_appradio_device_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/oplus_radio_device_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/oplus_wifi_supplicant_device.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/power-default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.mediatek.hardware.dplanner@2.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vibrator-mtk-default.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/realme/RMX2040/BoardConfigVendor.mk
